457d4ceb376d0f107d6ede13d3b1dd774f798247
Isaac Aymerich <isaac.aymerich@roche.com>
2026-06-24 11:26:24 +0200

    fix: mosquitto use CROSS_COMPILE to avoid double prefix

--- scripts/81_mosquitto.sh
+++ scripts/81_mosquitto.sh
@@ -24,9 +24,8 @@ echo "[*] Building mosquitto"
 make -C mosquitto \
   WITH_THREADING=no WITH_TLS=no WITH_CJSON=no WITH_BRIDGE=no WITH_PERSISTENCE=no \
   WITH_MEMORY_TRACKING=no WITH_DOCS=no WITH_STRIP=yes WITH_STATIC_LIBRARIES=yes \
-  WITH_SHARED_LIBRARIES=no CFLAGS="-Wall -Os" \
-  CC=arm-goke-linux-uclibcgnueabi-gcc CXX=arm-goke-linux-uclibcgnueabi-g++ \
-  AR=arm-goke-linux-uclibcgnueabi-ar
+  WITH_SHARED_LIBRARIES=no CROSS_COMPILE=arm-goke-linux-uclibcgnueabi- \
+  CC=gcc CXX=g++ AR=ar
 
 if [ ! -e "mosquitto/client/mosquitto_sub" ]; then
   echo "[!] Build failed!"
@@ -36,7 +35,6 @@ fi
 echo "[*] Installing"
 for NAME in mosquitto_sub mosquitto_pub; do
   cp mosquitto/client/${NAME} include/bin/${NAME}
-  arm-goke-linux-uclibcgnueabi-strip include/bin/${NAME}
 done
 rm -rf mosquitto mosquitto.tar.gz
 echo "[*] mosquitto built successfully"
